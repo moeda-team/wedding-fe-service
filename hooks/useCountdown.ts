262c79ac0cba4abc92dafa2f40ad87d123ba9d6b
Rizky Naufal <naufalrizky79@gmail.com>
2026-04-28 20:50:35 +0700

    Initial Commit

---
+++ hooks/useCountdown.ts
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const useCountdown = (targetDate: string) => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetDate) - +new Date();
+
+    if (difference <= 0) return null;
+
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [targetDate]);
+
+  return timeLeft;
+};
